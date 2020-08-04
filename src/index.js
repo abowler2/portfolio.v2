3c697bc6f4e47419e59cd85d6730d4e83c171901
abowler2 <abowler2@gmail.com>
2020-08-03 18:17:51 -0700

    reorganization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './Header';
+import Header from './components/header/Header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
