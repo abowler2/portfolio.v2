32790498214c8c4668f21688cfa66b56064c7077
abowler2 <abowler2@gmail.com>
2020-08-03 17:57:29 -0700

    set up header section

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+	return (
+		<header class="homepage-header">
+			<div>
+				<nav id="navbar">
+					<ul>
+						<li><a href="./work/index.html">Work</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+				<div class="intro">
+					<h1>Hello, my name is April.</h1>
+					<p>I'm a web developer based in California who enjoys creating beautiful websites.</p>
+				</div>
+			</div>
+		</header>
+  );
+}
