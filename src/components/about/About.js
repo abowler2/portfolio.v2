91fb18a79d01ff2097153b25cc585f3fa819e960
abowler2 <abowler2@gmail.com>
2020-08-04 15:52:11 -0700

    added additional projects and fixed styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -16,7 +16,9 @@ export default function About() {
                     </figure>
                     <div>
                         <h3>Find your passion, and it's no longer work. ~ <span>L.A. Reid</span></h3>
-                        <p> My journey into tech started while helping my oldest daughter find the direction she wanted to go in for college. She began to take computer science classNamees in high school, and I was intrigued. After finding a free online course to learn the basics of coding I was hooked! From there I have been working diligently to learn and improve my coding skills.<br /><br />Let's work together to create your vision!</p>
+                        <p> My journey into tech started while helping my oldest daughter find the direction she wanted to go in for college. She began to take computer science classes in high school, and I was intrigued. After finding a free online course to learn the basics of coding I was hooked! From there I have been working diligently to learn and improve my coding skills.
+                            <br /><br />
+                            Let's work together to create your vision!</p>
                     </div>
                 </article>
             </div>
--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -12,7 +12,7 @@ export default function Header() {
         <section id="featured">
             <div className="container">
                 <div>
-                    <h2>Featured Work</h2>
+                    <h2 className="title">Featured Work</h2>
                 </div>
                 <div>
                     {/* Set className of 'row' in order to line up image and description on bigger screens */}
@@ -28,6 +28,9 @@ export default function Header() {
                             </h3>
                             <h4>{ firstFeature.language }</h4>
                             <p>{ firstFeature.description }</p>
+                            <p className="button" >
+                                <a href={ firstFeature.codeUrl } target="_blank" rel="noopener noreferrer">See the Code</a>
+                            </p>
                         </div>
                     </article>
                     <article className="row">
@@ -42,6 +45,9 @@ export default function Header() {
                             </h3>
                             <h4>{ secondFeature.language }</h4>
                             <p>{ secondFeature.description }</p>
+                            <p className="button">
+                                <a href={ secondFeature.codeUrl } target="_blank" rel="noopener noreferrer">See the Code</a>
+                            </p>
                         </div>
                     </article>
 
@@ -57,10 +63,13 @@ export default function Header() {
                             </h3>
                             <h4>{ thirdFeature.language }</h4>
                             <p>{ thirdFeature.description }</p>
+                            <p className="button" >
+                                <a href={ thirdFeature.codeUrl } target="_blank" rel="noopener noreferrer">See the Code</a>
+                            </p>
                         </div>
                     </article>
                 <div className="container">
-                    <h4>Click <a href="./work/index.html"> HERE </a>to see more work.</h4>
+                    <h4 className="see-more">Click <a href="./work/index.html"> HERE </a>to see more work.</h4>
                 </div>
                 </div>
             </div>
