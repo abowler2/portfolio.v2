5954db91dc4c7a177eec514251fa030d4037b3b5
abowler2 <abowler2@gmail.com>
2020-08-09 10:00:12 -0700

    Updates

---
+++ precache-manifest.9af169ccd32b0ca5917455823772c686.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9304ca3d66a6f192470856ce37c8e623",
+    "url": "/portfolio.v2/index.html"
+  },
+  {
+    "revision": "d0aefa76c35fbebeaa0a",
+    "url": "/portfolio.v2/static/css/main.f39e820b.chunk.css"
+  },
+  {
+    "revision": "0c082f84bda45fb324bc",
+    "url": "/portfolio.v2/static/js/2.e6c271fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio.v2/static/js/2.e6c271fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0aefa76c35fbebeaa0a",
+    "url": "/portfolio.v2/static/js/main.305eb785.chunk.js"
+  },
+  {
+    "revision": "88bdd9d2a039e8e76964",
+    "url": "/portfolio.v2/static/js/runtime-main.76372d11.js"
+  },
+  {
+    "revision": "8f5a6629249c72785fcfd68f908e4f01",
+    "url": "/portfolio.v2/static/media/blankCanvas.8f5a6629.png"
+  },
+  {
+    "revision": "ce8c99dfaea2da193f42bf93b3f3062e",
+    "url": "/portfolio.v2/static/media/electionMap.ce8c99df.png"
+  },
+  {
+    "revision": "0a932ca477b33104000865a14b44645b",
+    "url": "/portfolio.v2/static/media/hero.0a932ca4.jpg"
+  },
+  {
+    "revision": "fb63dd819544619e3eec991332fdd8a4",
+    "url": "/portfolio.v2/static/media/holstee-manifesto-full.fb63dd81.png"
+  },
+  {
+    "revision": "163a5e29970c62023d22f9a0a9b21bdd",
+    "url": "/portfolio.v2/static/media/holstee-manifesto.163a5e29.png"
+  },
+  {
+    "revision": "7bb9d654778aa8db109dac426a39425d",
+    "url": "/portfolio.v2/static/media/jubilee-austen-full.7bb9d654.png"
+  },
+  {
+    "revision": "f0e1f44a515af764f4a4989dfc6fae3f",
+    "url": "/portfolio.v2/static/media/jubilee-austen.f0e1f44a.png"
+  },
+  {
+    "revision": "d756b24491199d7fc84b3fc8edcd0c19",
+    "url": "/portfolio.v2/static/media/logo.d756b244.png"
+  },
+  {
+    "revision": "d9ddfc836a546038e6fd7c0c6d9f0a2e",
+    "url": "/portfolio.v2/static/media/lolcat-clock.d9ddfc83.png"
+  },
+  {
+    "revision": "237e057118ca8422ae3b7bd6408a0f20",
+    "url": "/portfolio.v2/static/media/magic8Ball.237e0571.png"
+  },
+  {
+    "revision": "c4f77e777f8eac197e9a71c056a499d7",
+    "url": "/portfolio.v2/static/media/me.c4f77e77.jpg"
+  },
+  {
+    "revision": "f36a2415e25ea5005a71e66402ad99a4",
+    "url": "/portfolio.v2/static/media/nameTag.f36a2415.png"
+  },
+  {
+    "revision": "3c7e2f9612aaeb08522044c506908246",
+    "url": "/portfolio.v2/static/media/page-header.3c7e2f96.png"
+  },
+  {
+    "revision": "b627e0ec962f94e28951bfca16ceb2ed",
+    "url": "/portfolio.v2/static/media/pontoon.b627e0ec.png"
+  },
+  {
+    "revision": "e7fa0ec71252c725f62dbda078ed836e",
+    "url": "/portfolio.v2/static/media/reactMovie.e7fa0ec7.png"
+  },
+  {
+    "revision": "8671d4cad7dbabfab7f8d5a74ff91b8b",
+    "url": "/portfolio.v2/static/media/roguePickings.8671d4ca.png"
+  },
+  {
+    "revision": "2c39897d533f26c77ff5f5b75c6d7992",
+    "url": "/portfolio.v2/static/media/stickyNotes.2c39897d.png"
+  },
+  {
+    "revision": "0f735e03f4a8f675dd7c58106ee7a883",
+    "url": "/portfolio.v2/static/media/tribute-page.0f735e03.png"
+  },
+  {
+    "revision": "d2b8e4d00ef4bcb7ad945a0c27df2114",
+    "url": "/portfolio.v2/static/media/unplugged.d2b8e4d0.png"
+  },
+  {
+    "revision": "77b2c21f4b443cd42097bcbf9518e77d",
+    "url": "/portfolio.v2/static/media/work-page-header.77b2c21f.jpg"
+  }
+]);
