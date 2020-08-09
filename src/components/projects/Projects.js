ef9ee952129d7caa66631c8dd8ea2c38c101e980
abowler2 <abowler2@gmail.com>
2020-08-08 19:36:16 -0700

    added portal for project details

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './Projects.css';
 import projects from '../../projects.json';
 
@@ -10,10 +12,56 @@ export default function Projects() {
                 {projects.map(project => (
                     <div className="project-card" key={ project.id }>
                         <h4>{ project.title }</h4>
-                        <img src={ require(`../../img/${project.image}`) } alt={ project.title } />
+                        <ToggleContent
+                            toggle = { show => 
+                                <img 
+                                    src={ require(`../../img/${project.image}`) } 
+                                    alt={ project.title } 
+                                    onClick={show}
+                                />
+                            }
+                            content={hide => (
+                                <Modal>
+                                    <button className="modal-close" onClick={ hide }>
+                                        Close
+                                    </button>
+                                    <div>
+                                        <h2 className="modal-title">{ project.title }</h2>
+                                        <p>{ project.language }</p>
+                                        <p>{ project.description }</p>
+                                    </div>
+                                </Modal>
+                            )}
+                        />
                     </div>
                 ))}
             </div>
         </section>
     )
 }
+
+const ToggleContent = ({ toggle, content }) => {
+    const [isShown, setIsShown] = useState(false);
+    const hide = () => setIsShown(false);
+    const show = () => setIsShown(true);
+
+    return (
+        <React.Fragment>
+            {toggle(show)}
+            {isShown && content(hide)}
+        </React.Fragment>
+    );
+}
+
+const Modal = ({ children }) => (
+    ReactDOM.createPortal(
+        <React.Fragment>
+            <div className="modal-container">
+                <div className="modal">
+                    { children }
+                </div>
+            </div>
+        </React.Fragment>,
+        document.getElementById('modal-root')
+    )
+);
